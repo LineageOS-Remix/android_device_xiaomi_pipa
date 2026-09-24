7f74c05840b5579b52355d2c1c45b5160b80457e
CuriousNom <aryanbiswas7585@gmail.com>
2026-09-24 08:46:36 +0530

    pipa: Setup device dependencies
    Change-Id: I7a8a96b9ff353a6f9ea9485022da1e2c56df317a

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+git clone -b lineage-22.2 https://github.com/LineageOS-Remix/android_device_xiaomi_sm8250-common.git device/xiaomi/sm8250-common
+git clone -b lineage-22.2 --depth 1 https://github.com/LineageOS-Remix/proprietary_vendor_xiaomi_pipa.git vendor/xiaomi/pipa
+git clone -b 17 --depth 1 https://gitlab.com/CuriousNom/proprietary_vendor_xiaomi_pipa-firmware.git vendor/xiaomi/pipa-firmware
+git clone -b lineage-22.2 --depth 1 https://github.com/LineageOS-Remix/proprietary_vendor_xiaomi_sm8250-common.git vendor/xiaomi/sm8250-common
+git clone -b staging --depth 1 https://github.com/CuriousNom/n0_kernel_pipa kernel/xiaomi/sm8250
+git clone -b lineage-22.2 https://github.com/LineageOS-Remix/android_hardware_xiaomi.git hardware/xiaomi
